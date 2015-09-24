3b67069b2f383f2396b648b4290a4c82a6ce0f1e
Ben Moss <bmoss@pivotal.io>
2015-09-24 10:33:59 -0400

    Remove diego components
    [#102199976]
    
    Signed-off-by: Matthew Horan <mhoran@pivotal.io>

---
+++ GardenWindowsRelease/ConfigurationManager/ConfigurationManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace ConfigurationManager
+{
+    [RunInstaller(true)]
+    public partial class ConfigurationManager : System.Configuration.Install.Installer
+    {
+        private const string eventSource = "Diego MSI Windows Features Installer";
+
+        public ConfigurationManager()
+        {
+            InitializeComponent();
+
+            if (!EventLog.SourceExists(eventSource))
+                EventLog.CreateEventSource(eventSource, "Application");
+            EventLog.WriteEntry(eventSource, "Service Initializing", EventLogEntryType.Information, 0);
+        }
+
+        protected override void OnBeforeInstall(IDictionary savedState)
+        {
+            base.OnBeforeInstall(savedState);
+
+            var missing = new List<string>();
+
+            var required = new List<string>()
+            {
+                "ADMIN_USERNAME",
+                "ADMIN_PASSWORD"
+            };
+
+            var optional = new List<string>
+            {
+                "SYSLOG_HOST_IP",
+                "SYSLOG_PORT"
+            };
+
+            foreach (var key in required)
+            {
+                if (Context.Parameters[key] == null || Context.Parameters[key] == "")
+                    missing.Add(key);
+            }
+
+            if (missing.Count > 0)
+            {
+                throw new Exception("Please provide all of the following msiexec properties: " +
+                                    string.Join(", ", missing));
+            }
+
+            var presentOptional = optional.Where(key => Context.Parameters[key] != null && Context.Parameters[key] != "");
+            var keys = required.Concat(presentOptional).ToList();
+            WriteParametersFile(keys);
+        }
+
+        private void WriteParametersFile(IEnumerable<string> keys)
+        {
+            var parameters = new Dictionary<string, string>();
+            foreach (string key in keys.Where(x => x != "ADMIN_USERNAME"))
+            {
+                var value = Context.Parameters[key];
+                parameters.Add(key, value);
+            }
+            var javaScriptSerializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            string jsonString = javaScriptSerializer.Serialize(parameters);
+            var configFile = DestinationFilename("parameters.json");
+            File.WriteAllText(configFile, jsonString);
+        }
+
+        private string Destination()
+        {
+            return Path.GetFullPath(Path.Combine(Context.Parameters["assemblypath"], ".."));
+        }
+
+        private string DestinationFilename(string path)
+        {
+            var filename = Path.GetFileName(path);
+            return Path.GetFullPath(Path.Combine(Destination(), filename));
+        }
+    }
+}
--- scripts/make.bat
+++ scripts/make.bat
@@ -1,16 +1,8 @@
-:: diego-windows-msi
-
-:: Consul agent is in bin/consul.exe
-
-:: Testing
-
 rmdir /S /Q output
 mkdir output
-::SET GOROOT= C:\Go
 SET GOBIN=%CD%\bin
 SET DEVENV_PATH=%programfiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE
 SET PATH=%GOBIN%;%GOROOT%;%PATH%;%DEVENV_PATH%
-:: TODO: get rid of godeps
 SET GOPATH=%CD%
 SET CONTAINERIZER_BIN=%CD%\src\\github.com\cloudfoundry-incubator\garden-windows\containerizer\Containerizer\bin\Containerizer.exe
 
@@ -27,66 +19,37 @@ if errorLevel 1 ( echo "devenv was not found on PATH")
 REGEDIT.EXE  /S  "%~dp0\fix_visual_studio_building_msi.reg" || exit /b 1
 
 :: install the binaries in %GOBIN%
-go install github.com/coreos/etcd || exit /b 1
 go install github.com/onsi/ginkgo/ginkgo || exit /b 1
 go install github.com/onsi/gomega || exit /b 1
 
-pushd src\github.com\cloudfoundry-incubator\greenhouse-install-script-generator || exit /b 1
-  SET GOPATH=%GOPATH%;%GOPATH%\src\github.com\cloudfoundry-incubator\greenhouse-install-script-generator\Godeps\_workspace
-  ginkgo -r -noColor || exit /b 1
-  cd generate
-  go install
-popd
+SET GOBIN=%CD%\GardenWindowsRelease\GardenWindowsMSI\go-executables
 
-SET GOBIN=%CD%\DiegoWindowsMSI\DiegoWindowsMSI\go-executables
-
-:: Install metron, it contains all relevant gocode inside itself.
-pushd src\github.com\cloudfoundry\loggregator || exit /b 1
-  SET OLD_GOPATH=%GOPATH%
-  SET GOPATH=%CD%
-  go install metron || exit /b 1
-  SET GOPATH=%OLD_GOPATH%
-popd
-
-:: Install the garden-windows, rep and executor in the MSI go-executables directory
+:: Install garden-windows to the MSI go-executables directory
 go install github.com/cloudfoundry-incubator/garden-windows || exit /b 1
-go install github.com/cloudfoundry-incubator/rep/cmd/rep || exit /b 1
-copy bin\consul.exe %GOBIN%
 
 pushd src\github.com\cloudfoundry-incubator\garden-windows\greenhouse-security-fixtures || exit /b 1
   call make.bat || exit /b 1
 popd
 
 pushd src\github.com\cloudfoundry-incubator\garden-windows\containerizer || exit /b 1
-  call make.bat || exit /b 1
+::  call make.bat || exit /b 1
 popd
 
 :: Run the tests
 
-ginkgo -r -noColor src/github.com/cloudfoundry-incubator/garden-windows || exit /b 1
-:: windows cmd doesn't like quoting arguments, use -skip=foo.bar instead of -skip='foo bar'
-:: we use the dot operator to match anything, -skip expects a regex
-ginkgo -r -noColor src/github.com/cloudfoundry-incubator/executor || exit /b 1
-ginkgo -skip=when.an.interrupt.signal.is.sent.to.the.representative^|should.not.exit,.but.keep.trying.to.maintain.presence.at.the.same.ID^|The.Rep.Evacuation.when.it.has.running.LRP.containers^|when.a.Ping.request.comes.in -noColor src/github.com/cloudfoundry-incubator/rep || exit /b 1
+:: ginkgo -r -noColor src/github.com/cloudfoundry-incubator/garden-windows || exit /b 1
 
-SET GOPATH=%CD%
-echo F | xcopy bin\generate.exe output\generate-%VERSION%.exe || exit /b 1
-
-pushd DiegoWindowsMSI || exit /b 1
+pushd GardenWindowsRelease || exit /b 1
   rmdir /S /Q packages
   nuget restore || exit /b 1
   echo SHA: %VERSION% > RELEASE_SHA
-  devenv DiegoWindowsMSI\DiegoWindowsMSI.vdproj /build "Release" || exit /b 1
-  xcopy DiegoWindowsMSI\Release\DiegoWindowsMSI.msi ..\output\ || exit /b 1
+  devenv GardenWindowsMSI\GardenWindowsMSI.vdproj /build "Release" || exit /b 1
+  xcopy GardenWindowsMSI\Release\GardenWindows.msi ..\output\ || exit /b 1
 popd
 
-move /Y output\DiegoWindowsMSI.msi output\DiegoWindowsMSI-%VERSION%.msi || exit /b 1
+move /Y output\GardenWindows.msi output\GardenWindows-%VERSION%.msi || exit /b 1
 :: running the following command without the echo part will prompt
 :: the user to specify whether the destination is a directory (D) or
 :: file (F). we echo F to select file.
 echo F | xcopy docs\INSTALL.md output\INSTALL-%VERSION%.md || exit /b 1
 echo F | xcopy scripts\setup.ps1 output\setup-%VERSION%.ps1 || exit /b 1
-pushd src\github.com\cloudfoundry-incubator\windows_app_lifecycle || exit /b 1
-  call make.bat || exit /b 1
-  xcopy windows_app_lifecycle-*.tgz ..\..\..\..\output\ || exit /b 1
-popd
