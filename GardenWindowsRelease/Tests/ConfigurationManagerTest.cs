164ca0e57b4f2090f304968623dcb0c4d6cd1c6d
Anthony Emengo <aemengo@pivotal.io>
2015-11-12 14:19:00 -0500

    Add a test for writing the parameters.json file
    Signed-off-by: Ben Moss <bmoss@pivotal.io>

---
+++ GardenWindowsRelease/Tests/ConfigurationManagerTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Configuration.Install;
+using System.IO;
+using Ploeh.AutoFixture.Xunit2;
+using Xunit;
+
+namespace Tests
+{
+    public class ConfigurationManagerTest : ConfigurationManager.ConfigurationManager
+    {
+        [Theory, AutoData]
+        public void ItPersistsArgumentsInFile(
+            string adminUsername, string adminPassword, string containerDirectory, string externalIp, string syslogHostIp, string syslogPort)
+        {
+            var tempDirectory = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName(), Path.GetRandomFileName());
+            Directory.CreateDirectory(tempDirectory);
+            var configurationManager = new ConfigurationManagerTest();
+            var context = new InstallContext();
+            context.Parameters.Add("ADMIN_USERNAME", adminUsername);
+            context.Parameters.Add("ADMIN_PASSWORD", adminPassword);
+            context.Parameters.Add("CONTAINER_DIRECTORY", containerDirectory);
+            context.Parameters.Add("EXTERNAL_IP", externalIp);
+            context.Parameters.Add("SYSLOG_HOST_IP", syslogHostIp);
+            context.Parameters.Add("SYSLOG_PORT", syslogPort);
+            context.Parameters.Add("assemblypath", tempDirectory);
+            configurationManager.Context = context;
+            configurationManager.OnBeforeInstall(null);
+
+            var javaScriptSerializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+            var jsonString = File.ReadAllText(Path.Combine(tempDirectory, @"..\parameters.json"));
+            var hash = javaScriptSerializer.Deserialize<Dictionary<string, string>>(jsonString);
+            Assert.False(hash.ContainsKey("ADMIN_PASSWORD"));
+            Assert.Equal(hash["CONTAINER_DIRECTORY"], containerDirectory);
+            Assert.Equal(hash["EXTERNAL_IP"], externalIp);
+            Assert.Equal(hash["SYSLOG_HOST_IP"], syslogHostIp);
+            Assert.Equal(hash["SYSLOG_PORT"], syslogPort);
+        }
+    }
+}
