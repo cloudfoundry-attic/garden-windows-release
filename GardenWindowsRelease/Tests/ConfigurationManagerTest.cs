4a3228588511f3717f3f1951575795b0e863d3b7
Matthew Horan <mhoran@pivotal.io>
2016-05-05 16:26:59 -0400

    ACL GardenWindows ProgramData directory for Administrators only
    [#118793299]
    
    Signed-off-by: Luan Santos <lsantos@pivotal.io>

--- GardenWindowsRelease/Tests/ConfigurationManagerTest.cs
+++ GardenWindowsRelease/Tests/ConfigurationManagerTest.cs
@@ -1,26 +1,25 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Configuration.Install;
 using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Principal;
 using Ploeh.AutoFixture.Xunit2;
-using Utilities;
 using Xunit;
+using System.Security.AccessControl;
 
 namespace Tests
 {
-    public class TempDirectory : IDisposable
+    public class TempDirectory
     {
         private string path;
 
         public TempDirectory()
         {
-            path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName(), Path.GetRandomFileName());
+            path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
             Directory.CreateDirectory(path);
-        }
-
-        public void Dispose()
-        {
-            Directory.Delete(Path.Combine(path, "../"), true);
         }
 
         public override string ToString()
@@ -28,22 +27,42 @@ namespace Tests
             return path;
         }
     }
-    public class ConfigurationManagerTest : ConfigurationManager.ConfigurationManager
+
+    public class ConfigurationManagerWithOverridableDestinationPath : ConfigurationManager.ConfigurationManager
     {
-        private string destinationPath { get; set; }
+        public string destinationPath { get; set; }
 
         protected override string Destination()
         {
             return destinationPath;
         }
 
+        public new void OnBeforeInstall(IDictionary savedState)
+        {
+            base.OnBeforeInstall(savedState);
+        }
+    }
+
+    public class ConfigurationManagerTest : IDisposable
+    {
+        private ConfigurationManagerWithOverridableDestinationPath configurationManager;
+        private TempDirectory tempDirectory = new TempDirectory();
+
+        public ConfigurationManagerTest()
+        {
+            configurationManager = new ConfigurationManagerWithOverridableDestinationPath();
+            configurationManager.destinationPath = tempDirectory.ToString();
+        }
+
+        public void Dispose()
+        {
+            Directory.Delete(tempDirectory.ToString(), true);
+        }
+
         [Theory, AutoData]
         public void ItPersistsArgumentsInFile(
             string containerDirectory, string machineIp, string syslogHostIp, string syslogPort, string machineName)
         {
-            TempDirectory tempDirectory = new TempDirectory();
-            var configurationManager = new ConfigurationManagerTest();
-            configurationManager.destinationPath = tempDirectory.ToString();
             var context = new InstallContext();
             context.Parameters.Add("CONTAINER_DIRECTORY", containerDirectory);
             context.Parameters.Add("MACHINE_IP", machineIp);
@@ -52,6 +71,14 @@ namespace Tests
             context.Parameters.Add("MACHINE_NAME", machineName);
             configurationManager.Context = context;
             configurationManager.OnBeforeInstall(null);
+
+            var acl = Directory.GetAccessControl(tempDirectory.ToString());
+            var accessRules = acl.GetAccessRules(true, true, typeof(SecurityIdentifier));
+            Assert.Equal(accessRules.Count, 1);
+            var rule = (FileSystemAccessRule)accessRules[0];
+            Assert.Equal(rule.AccessControlType, AccessControlType.Allow);
+            Assert.Equal(new SecurityIdentifier(WellKnownSidType.BuiltinAdministratorsSid, null), rule.IdentityReference);
+            Assert.Equal(rule.FileSystemRights, FileSystemRights.FullControl);
 
             var javaScriptSerializer = new System.Web.Script.Serialization.JavaScriptSerializer();
             var parametersPath = Path.Combine(tempDirectory.ToString(), "parameters.json");
